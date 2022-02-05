90405c8d45596cb160d8f4fb9c409da1d01002fb
Manava21 <91476361+Manava21@users.noreply.github.com>
2022-02-05 09:18:19 +0530

    Update tip_billsplit_calculator.py

--- tip_billsplit_calculator.py
+++ tip_billsplit_calculator.py
@@ -1,14 +1,19 @@
-total_bill = float(input("What was the total bill? $ "))
-tip = (input("What percentzge tip would you like to give? 10, 12, 15?"))
-bill_split = int(input("how many people to split the bill? "))
+package myjava;
 
+import java.util.Scanner;
 
-if(tip== "N"):
-# each_pay = round(float((total_bill / bill_split) * tip_percent),2) 
-# print(f"each person should pay : ${each_pay} ")
-  each_pay = round(float(total_bill / bill_split),2)
-  print(f" each person should pay : ${each_pay} ")
-else:
-  tip_percent = ((int(tip)/100) +  1) 
-  each_pay = round(float((total_bill / bill_split) * tip_percent),2)
-  print(f" each person should pay : ${each_pay} ")
+public class Myfour {
+
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+			Scanner scanner = new Scanner(System.in)) {
+	System.out.println("What was the total bill? $");
+	 float total_bill = scanner.nextFloat();
+	System.out.println("how many people to split the bill? ");
+	int bill_split = scanner.nextInt();
+	float each_pay =  (total_bill / bill_split);
+    System.out.println("each person should pay :" );
+	System.out.print(each_pay);					
+			}
+	}
+  
